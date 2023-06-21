1b9dcd8fa6e74bdc9066c81b9b12c3b72b65324c
Alaa M Ashraf <alaa.mohamed.ashraf92@gmail.com>
2023-06-20 19:13:49 -0700

    trying edit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,41 @@
-include "main.h"
-
-/**
- * print_times_table - Prints the n times table
- *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
- */
-void print_times_table(int n)
-{
-	int a, b, op;
-
-	if (n >= 0 && n <= 15)
+#include <stdio.h>
+	/**
+	 * main - fibonacci <3
+	 *
+	 * Purpose - no hardcode
+	 *
+	 * Return:  (Success)
+	 */
+	int main(void)
 	{
-		for (a = 0; a <= n; a++)
+		unsigned long int i;
+		unsigned long int bef = 1;
+		unsigned long int aft = 2;
+		unsigned long int l = 1000000000;
+		unsigned long int bef1;
+		unsigned long int bef2;
+		unsigned long int aft1;
+		unsigned long int aft2;
+		printf("%lu", bef);
+		for (i = 1; i < 91; i++)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
+			printf(", %lu", aft);
+			aft += bef;
+			bef = aft - bef;
 		}
+		bef1 = (bef / l);
+		bef2 = (bef % l);
+		aft1 = (aft / l);
+		aft2 = (aft % l);
+		for (i = 92; i < 99; ++i)
+		{
+			printf(", %lu", aft1 + (aft2 / l));
+			printf("%lu", aft2 % l);
+			aft1 = aft1 + bef1;
+			bef1 = aft1 - bef1;
+			aft2 = aft2 + bef2;
+			bef2 = aft2 - bef2;
+		}
+		printf("\n");
+		return (0);
 	}
-}
